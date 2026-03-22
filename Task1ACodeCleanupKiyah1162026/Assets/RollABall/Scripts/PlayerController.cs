05c5638b85f4877f256698c55390a360957c982e
Kiyah <132413293+Kxyah13@users.noreply.github.com>
2026-03-22 15:26:52 +1000

    Player Character Update
    Player Character has been changed from sphere to cube as per task request
    
    - Cube no longer rolls when keys 'WASD' are used
    - Glides with physics and does not spin when it hits walls

--- Task1ACodeCleanupKiyah1162026/Assets/RollABall/Scripts/PlayerController.cs
+++ Task1ACodeCleanupKiyah1162026/Assets/RollABall/Scripts/PlayerController.cs
@@ -1,45 +1,52 @@
-﻿// Player Controller Script
-using UnityEngine;
+﻿using UnityEngine;
+
+// Include the namespace required to use Unity UI
 using UnityEngine.UI;
 using System.Collections;
+public class PlayerController : MonoBehaviour {
 
-public class PlayerController : MonoBehaviour{
+    // Create public variables for player speed, and for the Text UI game objects
+    [SerializeField]
+    public float f_horPlayAccel;
 
-    // Public variables player speed, and for the Text UI game objects
-	public float f_horPlayAccel;
 	public Text NumTotal;
 	public Text wintext;
 
-	// Private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
 	private Rigidbody rb;
 	private float inttotalcount;
 
-	// Game Start
+	// At the start of the game..
 	void Start ()	{rb = GetComponent<Rigidbody>();inttotalcount = 0;SetCountText ();wintext.text = "";}
+
 	void FixedUpdate ()
 	{
-		// Local float variables equal to the value of our Horizontal and Vertical Inputs
+		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-		//Vector3 variable, and assign X and Z to feature horizontal and vertical float variables above
+		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.AddForce (movement * f_horPlayAccel);
 	}
 	void OnTriggerEnter(Collider other) 
-{
-if (other.gameObject.CompareTag ("Pick Up"))
-{	other.gameObject.SetActive (false);
-		inttotalcount = inttotalcount + 1;
-		SetCountText ();
-}}
+	{
+		Debug.Log("Working");
+	if (other.gameObject.CompareTag ("Pick Up"))
+		{			
+				other.gameObject.SetActive (false);
+				inttotalcount = inttotalcount + 1;
+				SetCountText ();
+		}
+	}
 
-	//Counter UI setup/checker
     void SetCountText()
-	{NumTotal.text = "Count: " + inttotalcount.ToString ();
+{NumTotal.text = "Count: " + inttotalcount.ToString ();
 		if (inttotalcount >= 12) 
-{
-			// Text value of 'winText'
+	{
+			// Set the text value of our 'winText'
 			wintext.text = "You Win!";
-}
-}
-}
+
+
+	}
+	}
+	}
